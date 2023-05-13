b8645fc6433783ea12b38e2da3fd31344283ebc2
pato <patooodv@gmail.com>
2023-05-13 10:56:46 -0300

    se termina de agregar los metodos de grafo

--- trabajoPractico3/src/tp3/GrafoDirigido.java
+++ trabajoPractico3/src/tp3/GrafoDirigido.java
@@ -2,28 +2,32 @@ package tp3;
 
 import java.util.Iterator;
 import java.util.Map;
-
-import Tp1.ej5.MyIterator;
-
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class GrafoDirigido<T> implements Grafo<T> {
 
-	private Map<Integer, ArrayList<Arco>> grafo = new HashMap<Integer, ArrayList<Arco>>();
+	private Map<Integer, ArrayList<Arco<T>>> grafo = new HashMap<Integer, ArrayList<Arco<T>>>();
 	
 	@Override
 	public void agregarVertice(int verticeId) {
-		if(grafo.get(verticeId) == null) {
-			grafo.put(verticeId,new ArrayList<Arco>());
+		if(!contieneVertice(verticeId)) {
+			grafo.put(verticeId,new ArrayList<Arco<T>>());
 		}
 	}
 
 	@Override
 	public void borrarVertice(int verticeId) {
-		// borrar los vertices origen de este vertice que nos llega
-		// los vertices destino de este vertice(los adyacentes ) se borran automaticamente
-
+		if(contieneVertice(verticeId)) {
+			this.grafo.remove(verticeId);
+			Iterator<Arco<T>> arcos = obtenerArcos();
+			while(arcos.hasNext()) {
+				if(arcos.next().getVerticeDestino() == verticeId ) {
+					borrarArco(arcos.next().getVerticeOrigen(), arcos.next().getVerticeDestino());
+				}
+			}
+		}
 	}
 
 	@Override
@@ -46,10 +50,7 @@ public class GrafoDirigido<T> implements Grafo<T> {
 	@Override
 	public void borrarArco(int verticeInicio, int verticeDestino) {
 		if(existeArco(verticeInicio, verticeDestino)) {
-			// arreglar
-			ArrayList<Arco> arcos = grafo.get(verticeInicio);
-			Arco arcoNuevo = new Arco(verticeInicio, verticeDestino, null);
-			arcos.remove(arcoNuevo);
+			this.grafo.get(verticeInicio).remove(obtenerArco(verticeInicio, verticeDestino));
 		}
 	}
 
@@ -63,72 +64,110 @@ public class GrafoDirigido<T> implements Grafo<T> {
 		// primero compruebo que los vertices existan
 		if(contieneVertice(verticeInicio) && contieneVertice(verticeDestino)) {
 			// obtengo la arraylist de arcos a partir del verticeInicio del grafo
-			ArrayList<Arco> arcos = grafo.get(verticeInicio);
-			// compruebo que la Arraylist no este vacia
-			if(!arcos.isEmpty()) {
-				// bien pero mal O(n2)
-				for (int i = 0; i < arcos.size(); i++) {
-					// si el vertice de origen y destino del arco son iguales al de los paremetros retorno true
-					if(arcos.get(i).getVerticeOrigen() == verticeInicio && arcos.get(i).getVerticeDestino() == verticeDestino) {
+			ArrayList<Arco<T>> arcos = grafo.get(verticeInicio);
+			
+				// con iterator
+				Iterator<Arco<T>> aux = arcos.iterator();
+				while(aux.hasNext()) {
+					Arco<T> arco = (Arco<T>) aux.next();
+					if(arco.getVerticeDestino() == verticeDestino) {
 						return true;
 					}
 				}
-				// hacer con iterator
-//				if(){
-//					
+				
+				// bien pero mal O(n2)
+//				for (int i = 0; i < arcos.size(); i++) {
+//					// si el vertice de origen y destino del arco son iguales al de los paremetros retorno true
+//					if(arcos.get(i).getVerticeOrigen() == verticeInicio && arcos.get(i).getVerticeDestino() == verticeDestino) {
+//						return true;
+//					}
 //				}
-			}
+			
 		}
 		return false;
 	}
 
 	@Override
-	public Arco<T> obtenerArco(int verticeId1, int verticeId2) {
-		// TODO Auto-generated method stub
+	public Arco<T> obtenerArco(int verticeInicio, int verticeDestino) {
+		if(contieneVertice(verticeInicio) && contieneVertice(verticeDestino) && existeArco(verticeInicio, verticeDestino)) {
+			ArrayList<Arco<T>> arcos = grafo.get(verticeInicio);
+			Iterator<Arco<T>> aux = arcos.iterator();
+			while(aux.hasNext()) {
+				Arco<T> arco = (Arco<T>) aux.next();
+				if(arco.getVerticeDestino() == verticeDestino) {
+					return arco;
+				}
+			}
+		}
 		return null;
 	}
 
 	@Override
 	public int cantidadVertices() {
-		// TODO Auto-generated method stub
-		return 0;
+		return grafo.size();
 	}
 
 	@Override
 	public int cantidadArcos() {
-		// TODO Auto-generated method stub
-		return 0;
+		// creo una variable vacia
+		int cantArcos = 0;
+		// obtengo una lista con todos los vertices del grafo y lo hago iterable
+		Iterator<Integer> vertices = this.grafo.keySet().iterator();
+		while(vertices.hasNext()) {
+			// a cantAcos le sumo la cantidad de arcos de cada vertice mientras que exista
+			cantArcos += this.grafo.get(vertices.next()).size();
+		}
+		return cantArcos;
 	}
 
 	@Override
 	public Iterator<Integer> obtenerVertices() {
-		// TODO Auto-generated method stub
-		return null;
+		//creo una lista vacia a la cual le voy a agregar todos los vertices del grafo
+		ArrayList<Integer> vertices = new ArrayList<>();
+		// key set devuelve una lista(no accecible)  de las claves del grafo
+		// a vertices le hago addall ya que this.grafo.keySet devuelve una lista con todos los vertices
+		vertices.addAll(this.grafo.keySet());
+		return vertices.iterator();
 	}
 
 	@Override
 	public Iterator<Integer> obtenerAdyacentes(int verticeId) {
-		// TODO Auto-generated method stub
-		return null;
+		//creo una lista vacia a la cual le voy a agregar los adyacentes
+		ArrayList<Integer> adyacentes = new ArrayList<>();
+		//obtengo los arcos del vertice que me llega por parametro
+		if(contieneVertice(verticeId)) {
+			ArrayList<Arco<T>> arcos = this.grafo.get(verticeId);
+			//lo transformo en interable 
+			Iterator<Arco<T>> aux = arcos.iterator();
+			while(aux.hasNext()) {
+				//al arraylist adyacentes le agrego todos los vertices adyacentes que existan
+				adyacentes.add(aux.next().getVerticeDestino());
+			}			
+		}
+		return adyacentes.iterator();
 	}
 
 	@Override
 	public Iterator<Arco<T>> obtenerArcos() {
-		// TODO Auto-generated method stub
-		return null;
+		//creo una lista vacia a la cual le voy a agregar los arcos
+		ArrayList<Arco<T>> arcos = new ArrayList<>();
+		// obtengo todos los vertices (en una lista) y lo hago iterable con .iterator()
+		Iterator<Integer> vertices = this.grafo.keySet().iterator();
+		while(vertices.hasNext()) {
+			//a arcos le realizo addAll con cada lista de arcos de cada vertice
+			arcos.addAll(this.grafo.get(vertices.next()));
+		}
+		return arcos.iterator();
 	}
 
 	@Override
 	public Iterator<Arco<T>> obtenerArcos(int verticeId) {
-		// TODO Auto-generated method stub
-		return null;
+		//creo una lista vacia a la cual le voy a agregar los arcos
+		ArrayList<Arco<T>> arcos = new ArrayList<>();
+		//a arcos le realizo addAll con cada lista de arcos del vertice que llega por parametro
+		arcos.addAll(this.grafo.get(verticeId));
+		
+		return arcos.iterator();
 	}
 	
-	@Override
-	public Iterator<T> iterator() {
-		//genero un nuevo iterador que apunta al primer elemento
-		
-		return new MyIterator<>(grafo);
-	}
-
 }
--- trabajoPractico3/src/tp3/main.java
+++ trabajoPractico3/src/tp3/main.java
@@ -1,20 +1,48 @@
 package tp3;
 
+import java.util.Iterator;
+
 public class main {
 	public static <T> void main(String[] args) {
 		GrafoDirigido<T> grafo = new GrafoDirigido<>();	
-		
+	
 		grafo.agregarVertice(0);
-
 		grafo.agregarVertice(10);
+		grafo.agregarVertice(11);
+		grafo.agregarVertice(12);
+		grafo.agregarVertice(13);
+		grafo.agregarVertice(14);
 		
 		grafo.agregarArco(0,10,null);
+		grafo.agregarArco(0,11,null);
+		grafo.agregarArco(12,13,null);
+		grafo.agregarArco(12,14,null);
 		
-		System.out.println( grafo.existeArco(0, 10));
+//		System.out.println( grafo.existeArco(0, 10));
 		
-		grafo.borrarArco(0, 10);
+
+//		System.out.println(grafo.obtenerArco(0, 10));
+//		System.out.println( grafo.existeArco(0, 10));
+//		System.out.println( grafo.existeArco(0, 11));
+//		System.out.println( grafo.existeArco(12, 13));
+//		System.out.println( grafo.existeArco(12, 14));
+//		System.out.println(grafo.cantidadVertices());
+		System.out.println(grafo.cantidadArcos());
+//		grafo.borrarArco(0, 10);
+//		System.out.println(grafo.cantidadArcos());
+//		
+//		Iterator<Integer> adyacentes = grafo.obtenerAdyacentes(12);
+//		while(adyacentes.hasNext()) {
+//			System.out.println(adyacentes.next());
+//		}
 		
-		System.out.println( grafo.existeArco(0, 10));
-		
+		Iterator<Integer> vertices = grafo.obtenerVertices();
+		while(vertices.hasNext()) {
+			System.out.println(vertices.next());
+		}
+	
+//		Iterator<Arco<T>> arcos = grafo.obtenerArcos();
+//		Iterator<Arco<T>> arcosDeVertice = grafo.obtenerArcos(0);
+//		
 	}
 }
